c65abaa1ba409047dc39d4fc9fa36403e4792858
Avik <aviknandi@yahoo.com>
2019-05-28 00:38:24 +0530

    added user signin & signup api

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 const sequelize = require('./connection');
 const Recipe = require('./models/recipe')
 const Ingredient = require('./models/ingredient');
+const User = require('./models/user');
 const routerRecipe = require('./api/recipe');
+const authRouter = require('./api/auth');
 const bodyParsor = require('body-parser');
 app.use(bodyParsor.urlencoded({extended: false}))
 app.use(bodyParsor.json())
@@ -23,10 +25,12 @@ app.use('/recipe', routerRecipe);
 app.use('/database', (req, res)=>{
     Ingredient.sync();
     Recipe.sync();
+    User.sync();
     res.status(200).json({
         mgs: "I just Start Myself jarvis"
     })
 });
+app.use('/',authRouter);
 app.use((req, res,next) =>{
     const err = new Error("Page Not Found!");
     next(err)
---
+++ models/user.js
@@ -0,0 +1,29 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../connection');
+
+
+const User = sequelize.define('user', {
+    id:{
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    password:{
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+}) 
+
+    
+
+module.exports = User;
+
+
